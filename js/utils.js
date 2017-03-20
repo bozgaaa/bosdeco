39fcd2afa4b54b439a92fc3be836440eeda70fe9
Andrei Bozga <andrei.bozga@tobania.be>
2017-03-20 11:53:48 +0100

    googlemaps correction Remove h1 logoText

--- js/utils.js
+++ js/utils.js
@@ -1,18 +1,16 @@
-
-
- /*
+/*
  * btn go up
  * ===============================================================
  */
-$(function(){
-    $('#btntop').click(function() {
+$(function () {
+    $('#btntop').click(function () {
         $('html,body').animate({scrollTop: 0}, 'slow');
     });
 
-    $(window).scroll(function(){
-        if($(window).scrollTop()<900){
+    $(window).scroll(function () {
+        if ($(window).scrollTop() < 900) {
             $('#btntop').fadeOut();
-        }else{
+        } else {
             $('#btntop').fadeIn();
         }
     });
@@ -25,11 +23,9 @@ $(function(){
  */
 //setCarouselHeight('#carousel-testimonial');
 
-function setCarouselHeight(id)
-{
+function setCarouselHeight(id) {
     var slideHeight = [];
-    $(id+' .item').each(function()
-    {
+    $(id + ' .item').each(function () {
         // add all slide heights to an array
         slideHeight.push($(this).height());
     });
@@ -38,122 +34,114 @@ function setCarouselHeight(id)
     max = Math.max.apply(null, slideHeight);
 
     // set the slide's height
-    $(id+' .carousel-content').each(function()
-    {
-        $(this).css('height',max+'px');
+    $(id + ' .carousel-content').each(function () {
+        $(this).css('height', max + 'px');
     });
 }
 
- $(function () {
-     var testimonialsHtml = "";
-     $.map( testimonials, function( value, index ) {
+$(function () {
+    var testimonialsHtml = "";
+    $.map(testimonials, function (value, index) {
 
-         //console.log( "index: " + index + "; value.comment: " + value.comment + "value.signature: " + value.signature );
+        //console.log( "index: " + index + "; value.comment: " + value.comment + "value.signature: " + value.signature );
 
-         var html = ''+
-             '<div class="item">'+
-                '<div class="carousel-content">'+
-                    '<div>'+
-                        '<p>' + value.comment + '</p><br>' +
-                        '<span class="signatureComment">' + value.signature +'</span>' +
-                    '</div>' +
-                '</div>'+
-             '</div>';
+        var html = '' +
+            '<div class="item">' +
+            '<div class="carousel-content">' +
+            '<div>' +
+            '<p>' + value.comment + '</p><br>' +
+            '<span class="signatureComment">' + value.signature + '</span>' +
+            '</div>' +
+            '</div>' +
+            '</div>';
 
-         testimonialsHtml = testimonialsHtml + html;
-     });
-     
-     $("#testimonials").html(testimonialsHtml);
-     $("#testimonials div:first-child").addClass('active');
- });
+        testimonialsHtml = testimonialsHtml + html;
+    });
 
-    
+    $("#testimonials").html(testimonialsHtml);
+    $("#testimonials div:first-child").addClass('active');
+});
 
- /*
+
+/*
  * google maps
  * ===============================================================
  */
-if(window.google && document.getElementById("map-container")){
-    window.initMap = function(){
-        var var_location = new google.maps.LatLng(50.836899, 4.320567);
+function initMap() {
+    var var_location = new google.maps.LatLng(50.836899, 4.320567);
 
-        var var_mapoptions = {
-            center: var_location,
-            zoom: 15,
-            mapTypeId: google.maps.MapTypeId.HYBRID,
-            scrollwheel: false,
-            draggable: true,
-            panControl: true,
-            zoomControl: true,
-            mapTypeControl: true,
-            scaleControl: true,
-            streetViewControl: true,
-            overviewMapControl: true,
-            rotateControl: true
-        };
+    var var_mapoptions = {
+        center: var_location,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.HYBRID,
+        scrollwheel: false,
+        draggable: true,
+        panControl: true,
+        zoomControl: true,
+        mapTypeControl: true,
+        scaleControl: true,
+        streetViewControl: true,
+        overviewMapControl: true,
+        rotateControl: true
+    };
+    var map = new google.maps.Map(document.getElementById('map-container'), var_mapoptions);
 
-        var var_map = new google.maps.Map(document.getElementById("map-container"),
-            var_mapoptions);
+    // Create a marker and set its position.
+    var marker = new google.maps.Marker({
+        position: var_location,
+        map: map,
+        title: "Bos Deco"
+    });
 
-        // Create a marker and set its position.
-        var var_marker = new google.maps.Marker({
-            position: var_location,
-            map: var_map,
-            title:"Bos Deco"});
-
-        var_marker.setMap(var_map);	
-
-      }
-
-    google.maps.event.addDomListener(window, 'load', init_map);
 }
-  
 
 
 /*
  * google analytics
  * ===============================================================
  */
- var _gaq = _gaq || [];
+var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-21762857-4']);
 _gaq.push(['_trackPageview']);
 
-(function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+(function () {
+    var ga = document.createElement('script');
+    ga.type = 'text/javascript';
+    ga.async = true;
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ga, s);
 })();
 
 
+/*
+ * JSON data
+ * ===============================================================
+ */
+var testimonials = [
+    {
+        comment: "Je tenais à vous remercier pour le travail de restauration que vous avez réalisé sur notre fauteuil.",
+        signature: "Marie et Alain."
+    },
+    {
+        comment: "J'ai bien reçu les deux fauteuils. Très beau travail! Merci beaucoup!",
+        signature: "Fabienne Delogne."
+    },
+    {
+        comment: "Je tenais tout d'abord à vous remercier pour la rapidité et l'excellente qualité de votre travail!",
+        signature: "Mark"
+    },
+    {
+        comment: "Mille mercis encore pour le fauteuil que vous m'avez rapporté à Koekelberg.",
+        signature: "Bernard"
+    },
+    {
+        comment: "Je vous remercie pour votre excellent travail. Je recommande.",
+        signature: "Paul"
+    },
+    {
+        comment: "Une gentillesse extraordinaire et un travail de qualité.",
+        signature: "Valerie"
+    }
 
- /*
-  * JSON data
-  * ===============================================================
-  */
- var testimonials = [
-     {
-         comment:"Je tenais à vous remercier pour le travail de restauration que vous avez réalisé sur notre fauteuil.",
-         signature:"Marie et Alain."
-     },
-     {
-         comment:"J'ai bien reçu les deux fauteuils. Très beau travail! Merci beaucoup!",
-         signature:"Fabienne Delogne."
-     },
-     {
-         comment:"Je tenais tout d'abord à vous remercier pour la rapidité et l'excellente qualité de votre travail!",
-         signature:"Mark"
-     },
-     {
-         comment:"Mille mercis encore pour le fauteuil que vous m'avez rapporté à Koekelberg.",
-         signature:"Bernard"
-     },
-     {
-         comment:"Je vous remercie pour votre excellent travail. Je recommande.",
-         signature:"Paul"
-     },
-     {
-         comment:"Une gentillesse extraordinaire et un travail de qualité.",
-         signature:"Valerie"
-     }
-
- ];
+];
